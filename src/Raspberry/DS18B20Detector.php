20d3e309039fb065b5d0a58e58d0303f3982cf43
Lewiatan <lewiatan.hpc@gmail.com>
2016-07-23 19:45:29 +0200

    DS12B20 Detector prototype

---
+++ src/Raspberry/DS18B20Detector.php
@@ -0,0 +1,23 @@
+<?php namespace Raspberry;
+
+use MrRio\ShellWrap as sh;
+
+class DS18B20Detector
+{
+    private static $path = '/sys/bus/w1/devices/w1_bus_master1/';
+    private static $pattern = '28-*/';
+
+    public static function detect()
+    {
+        $devices = self::getDevices();
+
+        return $devices;
+    }
+
+    private static function getDevices()
+    {
+        return sh::ls([
+            'd' => self::$path . self::$pattern
+        ]);
+    }
+}
